6d098e69d8c73549f95450520df405bcec0ce985
l7m1 <45667501+l7m1@users.noreply.github.com>
2021-10-23 20:35:54 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -38,7 +38,7 @@ def start():
         if sever == 'on' mess != 'Please Try Tomorrow' and mess != 'Checkin! Get 1 day':
             requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
         else:
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess1)
+            requests.get('https://sc.ftqq.com/' + sckey + '.send?text=yes'+)
     else:
         requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
 
