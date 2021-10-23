095860547d23896599a51838d61afed31d0c6fa8
l7m1 <45667501+l7m1@users.noreply.github.com>
2021-10-23 21:01:02 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -31,8 +31,11 @@ def start():
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         print(time)
-        if sever == 'on' :
+        if sever == 'on' and mess != 'Please Try Tomorrow' and mess != 'Checkin! Get 1 day':
             requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+        else:
+            requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+            
     else:
         requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
 
